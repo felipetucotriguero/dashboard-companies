f6bf3674543b28842cc001acc4504d16f7332a45
Felipe Tuco <felipetucot@gmail.com>
2024-11-03 21:49:51 -0400

    Total Suscribers Y lista de Integraciones + librerias nuevas instaladas en el package.json

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -3,6 +3,9 @@
 import { CardSummary } from "./components/CardSummary/CardSummary";
 import { BookOpenCheck, UserRound, Waypoints } from "lucide-react";
 import { LastCustomers } from "./components/LastCustomers";
+import { SalesDistributors } from "./components/SalesDistributors";
+import { TotalSuscribers } from "./components/TotalSuscribers";
+import { ListIntegrations } from "./components/ListIntegrations";
 
 export const dataCardsSummary = [
   {
@@ -48,7 +51,12 @@ export default function Home() {
 
       <div className="grid grid-cols-1 mt-12 xl:grid-cols-2 md:gap-x-10">
         <LastCustomers />
-      <p>Sales Distributors</p>
+        <SalesDistributors/>
+      </div>
+
+      <div className="flex-col md:gap-x-10 xl:flex xl:flex-row gap-y-4 md:gap-y-0 mt-12 md:mb-10 justify-center">
+        <TotalSuscribers />
+        <ListIntegrations/>
       </div>
     </div>
   );
